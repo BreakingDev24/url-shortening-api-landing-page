4749e53a615f759f93df2b04cddc18671674e87e
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-04-25 15:56:03 +0200

    feat(Header): implement responsive navigation menu toggle functionality
    -Added state management for the navigation menu to allow it to open and close when the menu button is clicked.
    -Added fuction toggleMenu
    -Updated the styles to ensure the menu displays correctly when active. This enhances user experience by providing a dynamic navigation interface.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,24 @@
 import logo from "../../assets/logo.svg";
 import style from "./Header.module.scss";
 import menuIcon from "../../assets/burger-menu.svg";
+import { useState } from "react";
 
 export default function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen((prev) => !prev); // Cambia lo stato attuale di isMenuOpen.
+    console.log(isMenuOpen);
+  };
+
   return (
     <header>
       <img className={style.logo} src={logo} alt="logo" />
-      <div className={style.navElements}>
+      <div
+        className={`${style.navElements} ${
+          isMenuOpen ? `${style.active}` : ""
+        }`}
+      >
         <nav className={style.nav}>
           <ul>
             <li>Feautures</li>
@@ -19,7 +31,7 @@ export default function Header() {
           <button className={style.signUpBtn}>Sign Up</button>
         </div>
       </div>
-      <button className={style.menuBtn}>
+      <button className={style.menuBtn} onClick={toggleMenu}>
         <img className={style.menuIcon} src={menuIcon} alt="menu-icon" />
       </button>
     </header>
