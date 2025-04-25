9f19a84e87cf3a6a53a377c24997e6071f04038a
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-04-25 10:31:02 +0200

    feat(config): enhance Vite configuration with SCSS support
    Added SCSS preprocessor options to the Vite configuration, including additional data imports for functions and variables. This change improves the styling workflow by allowing global access to SCSS resources.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { fileURLToPath } from "url"; // Importa fileURLToPath da 'url'
+import { dirname, resolve } from "path"; // Importa dirname e resolve da 'path'
 
-// https://vite.dev/config/
+// Calcola il percorso della cartella corrente
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "src"), // Alias '@' per 'src'
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @use "@/styles/functions" as *;
+          @use "@/styles/variables" as *;
+        `,
+      },
+    },
+  },
+});
