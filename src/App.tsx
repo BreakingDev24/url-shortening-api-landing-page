44696978b44fe4f8a8c476ac69f308246960f782
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-04-29 23:29:51 +0200

    feat(App): add About component to main layout
    - Included the About component in the main section of the App.
    - This change enhances the application by providing additional information to users.

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header></Header>
       <main>
         <Hero></Hero>
+        <About></About>
       </main>
     </>
   );
