2190e7b7bf29838d062794b2fdeaf2999714957d
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-05-08 11:18:38 +0200

    feat(FormSection): implement link shortening functionality
    - Added state management for input and saved links
    - Implemented getData function to fetch shortened links from the API
    - Updated handleSubmit to process form submission and call getData
    - Enhanced form to bind input value and handle changes

--- src/components/FormSection/FormSection.tsx
+++ src/components/FormSection/FormSection.tsx
@@ -1,13 +1,54 @@
+import { useEffect, useState } from "react";
 import Button from "../Button/Button";
 import style from "./FormSection.module.scss";
 
 export default function FormSection() {
+  const [input, setInput] = useState("");
+  // const [shortUrl, setShortUrl] = useState("");
+  const [savedLinks, setSavedLinks] = useState<
+    { userInput: string; shortned: string }[]
+  >([]);
+
+  useEffect(() => {
+    console.log("Links aggiornati:", savedLinks);
+  }, [savedLinks]);
+
+  async function getData(url: string) {
+    const corsProxy = "https://cors-anywhere.herokuapp.com/";
+    const apiUrl = "https://cleanuri.com/api/v1/shorten";
+    try {
+      const response = await fetch(corsProxy + apiUrl, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ url }),
+      });
+      const data = await response.json();
+      setSavedLinks((prev) => [
+        ...prev,
+        { userInput: url, shortned: data.result_url },
+      ]);
+    } catch (e) {
+      console.error(e);
+    }
+  }
+  const handleSubmit = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    console.log(input);
+    getData(input);
+  };
   return (
     <section className={style.formSection}>
       <div className="container">
         <div className={style.formContainer}>
-          <form action="">
-            <input type="text" placeholder="Shorten a link here..." />
+          <form action="" onSubmit={handleSubmit}>
+            <input
+              type="text"
+              placeholder="Shorten a link here..."
+              value={input}
+              onChange={(e) => setInput(e.target.value)}
+            />
             <Button variant="secondary" type="submit">
               Shorten It!
             </Button>
