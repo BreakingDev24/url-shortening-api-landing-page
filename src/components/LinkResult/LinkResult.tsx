2af354468759a30a868bedecda604d4bd37a3ba1
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-05-10 00:19:51 +0200

    feat(LinkResult): add LinkResult component for displaying user and short links - Implemented LinkResult component to show user-provided and shortened links - Added props for managing copy state and handling copy actions - Integrated Button component to allow users to copy the short link

---
+++ src/components/LinkResult/LinkResult.tsx
@@ -0,0 +1,34 @@
+import clsx from "clsx";
+import style from "./LinkResult.module.scss";
+import Button from "../Button/Button";
+
+interface LinkResultProps {
+  userLink: string;
+  shortLink: string;
+  index: number;
+  copied: boolean;
+  onCopy: (text: string, index: number) => void;
+}
+
+export default function LinkResult({
+  userLink,
+  shortLink,
+  index,
+  copied,
+  onCopy,
+}: LinkResultProps) {
+  return (
+    <div key={index} className={clsx(style.result)}>
+      <p className={clsx(style.userLink)}>{userLink}</p>
+      <div className={clsx(style.separator)}></div>
+      <p className={clsx(style.resultLink)}>{shortLink}</p>
+      <Button
+        copied={copied}
+        variant="secondary"
+        onClick={() => onCopy(shortLink, index)}
+      >
+        {copied ? "Copied" : "Copy"}
+      </Button>
+    </div>
+  );
+}
