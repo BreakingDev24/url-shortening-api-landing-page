2feae1551cc227f2e1f26792e77f2c046195447e
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-05-09 23:29:50 +0200

    feat(shortenApi): add getData function for URL shortening
    - Implemented the getData function to interact with the URL shortening API.
    - Utilizes a CORS proxy to handle cross-origin requests.
    - Sends a POST request with the URL to be shortened and returns the response data.

---
+++ src/api/shortenApi.ts
@@ -0,0 +1,19 @@
+async function getData(url: string) {
+  const corsProxy = "https://cors-anywhere.herokuapp.com/";
+  const apiUrl = "https://cleanuri.com/api/v1/shorten";
+  try {
+    const response = await fetch(corsProxy + apiUrl, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ url }),
+    });
+    const data = await response.json();
+    return data;
+  } catch (e) {
+    console.error(e);
+  }
+}
+
+export default getData;
