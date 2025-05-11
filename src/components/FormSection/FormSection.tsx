17e449f1d9be6cf7fb5e387d4fefcf55e3df1ea5
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-05-11 16:18:48 +0200

    feat(FormSection): add input validation and error handling
    - Introduced cleanAndValidateInputUrl function to validate user input URLs.
    - Added error state management to display error messages for invalid inputs.
    - Refactored handleSubmit to incorporate input validation before API call.

--- src/components/FormSection/FormSection.tsx
+++ src/components/FormSection/FormSection.tsx
@@ -3,35 +3,63 @@ import Button from "../Button/Button";
 import style from "./FormSection.module.scss";
 import getData from "../../api/shortenApi";
 import { useLinkContext } from "../../context/ShortenLinksContext";
+import clsx from "clsx";
 export default function FormSection() {
   const [input, setInput] = useState("");
   // const [shortUrl, setShortUrl] = useState("");
-  const { savedLinks, setSavedLinks } = useLinkContext();
+  const { savedLinks, setSavedLinks, error, setError } = useLinkContext();
 
   useEffect(() => {
     console.log("Links aggiornati:", savedLinks);
   }, [savedLinks]);
 
+  const cleanAndValidateInputUrl = (input: string): string | null => {
+    const trimmedInput = input.trim();
+    if (!trimmedInput) return null;
+    try {
+      const url = new URL(trimmedInput);
+      if (url.protocol !== "http:" && url.protocol !== "https:") return null;
+      const encodeUrl = encodeURI(trimmedInput);
+      return encodeUrl;
+    } catch (error) {
+      return null;
+    }
+  };
+
   const handleSubmit = async (e: { preventDefault: () => void }) => {
     e.preventDefault();
     console.log(input);
-    const data = await getData(input);
-    setSavedLinks((prev) => [
-      ...prev,
-      { userInput: input, shortned: data.result_url },
-    ]);
+    const cleanedInput = cleanAndValidateInputUrl(input);
+    if (!cleanedInput) {
+      setError("Please add a valid input");
+    }
+    try {
+      const data = await getData(input);
+      if (!data) return;
+      setSavedLinks((prev) => [
+        ...prev,
+        { userInput: input, shortned: data.result_url },
+      ]);
+    } catch (error) {
+      console.error(error);
+    }
   };
   return (
     <section className={style.formSection}>
       <div className="container">
         <div className={style.formContainer}>
           <form action="" onSubmit={handleSubmit}>
-            <input
-              type="text"
-              placeholder="Shorten a link here..."
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-            />
+            <div className={clsx(style.formGroup)}>
+              <input
+                type="text"
+                placeholder="Shorten a link here..."
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                className={clsx(error && style.error)}
+                required
+              />
+              {error && <p className={clsx(style.errorMsg)}>{error}</p>}
+            </div>
             <Button variant="secondary" type="submit">
               Shorten It!
             </Button>
