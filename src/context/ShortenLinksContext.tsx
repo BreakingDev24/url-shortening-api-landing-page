e368e0a0db47790b77f6ed0654fcced5fd401151
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-05-11 16:19:03 +0200

    feat(context): add error handling to ShortenLinksContext
    - Introduced error state and setter in LinkContextType
    - Updated LinkProvider to manage error state
    - Enhanced context functionality for better error management

--- src/context/ShortenLinksContext.tsx
+++ src/context/ShortenLinksContext.tsx
@@ -5,6 +5,8 @@ interface LinkContextType {
   setSavedLinks: React.Dispatch<
     React.SetStateAction<{ userInput: string; shortned: string }[]>
   >;
+  error: string;
+  setError: React.Dispatch<React.SetStateAction<string>>;
 }
 
 const ShortenLinkContext = createContext<LinkContextType | undefined>(
@@ -15,8 +17,11 @@ export function LinkProvider({ children }: { children: ReactNode }) {
   const [savedLinks, setSavedLinks] = useState<
     { userInput: string; shortned: string }[]
   >([]);
+  const [error, setError] = useState("");
   return (
-    <ShortenLinkContext.Provider value={{ savedLinks, setSavedLinks }}>
+    <ShortenLinkContext.Provider
+      value={{ savedLinks, setSavedLinks, error, setError }}
+    >
       {children}
     </ShortenLinkContext.Provider>
   );
