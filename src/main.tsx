ee72380964e48b441ef4b392f73b3e6bb39443e4
BreakingDev24 <102031294+BreakingDev24@users.noreply.github.com>
2025-05-12 16:24:16 +0200

    feat(InputRef): wrap App component with InputRefProvider for input focus management

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import { MenuProvider } from "./context/MenuContext.tsx";
 import "./styles/index.scss";
 import App from "./App.tsx";
 import { LinkProvider } from "./context/ShortenLinksContext.tsx";
+import { InputRefProvider } from "./context/InputRefContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <MenuProvider>
       <LinkProvider>
-        <App />
+        <InputRefProvider>
+          <App />
+        </InputRefProvider>
       </LinkProvider>
     </MenuProvider>
   </StrictMode>
